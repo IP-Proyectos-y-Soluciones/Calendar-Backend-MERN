043cee0f35be9b8a9aa3a25c1fe79288f35d9060
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-27 01:00:12 -0500

    Retrieve information from a post

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,34 @@
 import { response } from 'express';
 
 const createUser = ( req, res = response ) => {
+
+  const { name, email, password } = req.body;
+
+  if ( name.length < 5 ) {
+    return res.status( 400 ).json({
+      ok: false,
+      msg: 'Name must be longer than 5 characters',
+    });
+  };
+
   res.json({
     ok: true,
-    msg: 'Register'
+    msg: 'Register',
+    name,
+    email,
+    password,
   });
 };
 
 const loginUser = ( req, res = response ) => {
+
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
-    msg: 'Login'
+    msg: 'Login',
+    email,
+    password,
   });
 };
 
