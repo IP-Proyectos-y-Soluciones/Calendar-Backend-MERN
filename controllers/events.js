d2582a705a54ecef755d8ee89e5afdaa41c1ae64
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-27 09:32:01 -0500

    Get the list of Events

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,22 @@
 import { response } from 'express';
 import Event from '../models/Event.js';
 
-const getEvents = ( req, res = response ) => {
-  res.json({
-    ok: true,
-    msg: 'Get events'
-  });
+const getEvents = async( req, res = response ) => {
+  try {
+    const events = await Event.find().populate( 'user', 'name' );
+
+    res.json({
+      ok: true,
+      events
+    });
+  } catch ( error ) {
+    console.log( error );
+
+    res.status( 500 ).json({
+      ok: false,
+      msg: 'Contact the Administrator'
+    });
+  };
 };
 
 const createEvent = async( req, res = response ) => {
