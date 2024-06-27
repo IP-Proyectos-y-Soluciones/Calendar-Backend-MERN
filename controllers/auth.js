6f1120603a94b0999b4dc459b919f39cbdfd7d23
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-27 03:21:45 -0500

    Create user in the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,27 @@
 import { response } from 'express';
+import User from '../models/User.js';
 
-const createUser = ( req, res = response ) => {
+const createUser = async( req, res = response ) => {
 
-  const { name, email, password } = req.body;
+  // const { name, email, password } = req.body;
 
-  res.status( 201 ).json({
-    ok: true,
-    msg: 'Register',
-    name,
-    email,
-    password,
-  });
+  try {
+    const user = new User( req.body );
+  
+    // Guardar usuario
+    await user.save();
+  
+    res.status( 201 ).json({
+      ok: true,
+      msg: 'Register',
+    });
+  } catch ( error ) {
+    console.log( error );
+    res.status( 500 ).json({
+      ok: false,
+      msg: 'Contact administrator',
+    });
+  };
 };
 
 const loginUser = ( req, res = response ) => {
